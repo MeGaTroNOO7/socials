52e8f7e9906db4d64917881148796612dc7aa9f1
MeGaTroNOO7 <bernardgeorgecharles007@gmail.com>
2023-04-22 19:33:25 +0530

    init

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+import swal from 'sweetalert';
+
+swal("Hello world!");
 const discord = document.querySelector('.discord');
 const gmail = document.querySelector('.gmail');
 
-discord.addEventListener('click', () => alert('MEGATRON#7084'));
-gmail.addEventListener('click', () => alert('bernardgeorgecharles007@gmail.com'));
+discord.addEventListener('click', () => swal('MEGATRON#7084'));
+gmail.addEventListener('click', () => swal('bernardgeorgecharles007@gmail.com'));
